97929525e5a08b2bf3f577d176bd2c8d25777f8d
n0s3bleeed <allyssagarcia20@gmail.com>
2025-09-29 21:32:35 -0700

    ruby

--- src/main/java/net/ally/tutorialmod/TutorialMod.java
+++ src/main/java/net/ally/tutorialmod/TutorialMod.java
@@ -1,6 +1,8 @@
 package net.ally.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.ally.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -29,7 +31,7 @@ public class TutorialMod {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-
+        ModItems.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -43,7 +45,10 @@ public class TutorialMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey()== CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.RUBY);
+            event.accept(ModItems.RAW_RUBY);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/ally/tutorialmod/item/ModItems.java
@@ -0,0 +1,26 @@
+package net.ally.tutorialmod.item;
+
+import net.ally.tutorialmod.TutorialMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+     public static final RegistryObject<Item> RUBY = ITEMS.register("ruby",
+        ()-> new Item(new Item.Properties()));
+     public static final RegistryObject<Item> RAW_RUBY = ITEMS.register("raw_ruby",
+        ()-> new Item(new Item.Properties()));
+
+
+
+         public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
+
